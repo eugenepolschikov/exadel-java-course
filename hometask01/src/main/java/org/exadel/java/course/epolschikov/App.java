f263fd4d63fde3ed93ee71aa3ed835f47eac162d
epolschikov <ypolshchykau@exadel.com>
2014-11-11 15:00:34 +0300

    initial commit for hometask01

---
+++ hometask01/src/main/java/org/exadel/java/course/epolschikov/App.java
@@ -0,0 +1,13 @@
+package org.exadel.java.course.epolschikov;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
